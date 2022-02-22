e9f17aa5b918d642407e151a19d7ba085896df4a
baremikkel <75213352+baremikkel@users.noreply.github.com>
2022-02-22 16:50:28 +0100

    New version

--- Pok_mon_t_ller/Pok_mon_t_ller.ino
+++ Pok_mon_t_ller/Pok_mon_t_ller.ino
@@ -1,124 +1,61 @@
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin1 = 8;    // the number of the pushbutton pin
-const int buttonPin2 = 9;      // the number of the LED pin
-const int buttonPin3 = 10;
-const int buttonPin4 = 7;
+int aPin = A0;
+const int buttonPin = 13;
+int val = 0;
+int intervalMin[] = {0, 201, 401, 601, 801};
+int intervalMax[] = {200, 400, 600, 800, 1000};
+int counterdex[] = {1, 5, 10, 100, 1000};
 
-// Variables will change:
-int ledState = HIGH;         // the current state of the output pin
-int buttonState1;
-int buttonState2;
-int buttonState3;              // the current reading from the input pin
-int buttonState4;
-int lastButtonState1 = LOW;   // the previous reading from the input pin
-int lastButtonState2 = LOW;
-int lastButtonState3 = LOW;
-int lastButtonState4 = LOW;
-
-int k = 0;
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
-
-  lcd.begin(16, 2);
-
-  // set initial LED state
-//  digitalWrite(ledPin, ledState);
+int  buttonState;
+int lastButtonState = LOW;
+int counter = 0;
+int totalcount = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+void setup()
+{
+  lcd.init();                      // initialize the lcd
+  pinMode(buttonPin, INPUT);
 }
 
-void loop() {
-  // read the state of the switch into a local variable:
-  int reading1 = digitalRead(buttonPin1);
-  int reading2 = digitalRead(buttonPin2);
-  int reading3 = digitalRead(buttonPin3);
-  int reading4 = digitalRead(buttonPin4);
-
-  // If the switch changed, due to noise or pressing:
-  if (reading1 != lastButtonState1) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-  if (reading2 != lastButtonState2) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-  if (reading3 != lastButtonState3) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-  if (reading4 != lastButtonState4) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (reading1 != buttonState1) {
-      buttonState1 = reading1;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState1 == HIGH) {
-        k = k + 100;
-        lcd.clear();
-        lcd.print("Pokemons: ");
-        lcd.print(k);
-      }
-    }
-    if (reading2 != buttonState2) {
-      buttonState2 = reading2;
-      
-      if (buttonState2 == HIGH) {
-        k = k + 10;
-        lcd.clear();
-        lcd.print("Pokemons: ");
-        lcd.print(k);
-      }
-
-    }
-    if (reading3 != buttonState3) {
-      buttonState3 = reading3;
-      
-      if (buttonState3 == HIGH) {
-        k = k + 5;
-        lcd.clear();
-        lcd.print("Pokemons: ");
-        lcd.print(k);
-      }
-
-    }
-    if (reading4 != buttonState4) {
-      buttonState4 = reading4;
-      
-      if (buttonState4 == HIGH) {
-        k = k + 1;
-        lcd.clear();
-        lcd.print("Pokemons: ");
-        lcd.print(k);
-      }
-
+void loop()
+{
+  int reading = digitalRead(buttonPin);
+  val = analogRead(aPin);
+  for (int i = 0; i < 5; i++)
+  {
+    if (intervalMin[i] < val && val < intervalMax[i])
+    {
+      counter = counterdex[i];
     }
   }
+    if (reading != lastButtonState)
+      lastDebounceTime = millis();
 
-  // set the LED:
-//  digitalWrite(ledPin, ledState);
-
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState1 = reading1;
-  lastButtonState2 = reading2;
-  lastButtonState3 = reading3;
-  lastButtonState4 = reading4;
+    if ((millis() - lastDebounceTime) > debounceDelay)
+    {
+      if (reading != buttonState)
+      {
+        buttonState = reading;
+        if (buttonState == HIGH)
+        {
+          Serial.println(counter);
+          totalcount += counter;
+        }
+      }
+    }
+  lastButtonState = reading;
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Pokemon: ");
+  lcd.print(totalcount);
+  lcd.setCursor(0, 1);
+  lcd.print("Counter: ");
+  lcd.print(counter);
+  delay(100);
+  lcd.clear();
 }
